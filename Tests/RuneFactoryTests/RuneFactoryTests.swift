fd5f9a29406928dbdee1ac27d59caf3fa267ed32
Steve Trewick <steve.trewick@icloud.com>
2020-07-30 19:47:15 +0100

    remove default test

--- Tests/RuneFactoryTests/RuneFactoryTests.swift
+++ Tests/RuneFactoryTests/RuneFactoryTests.swift
@@ -2,14 +2,6 @@ import XCTest
 @testable import RuneFactory
 
 final class RuneFactoryTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RuneFactory().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    static var allTests : [(String, (RuneFactoryTests) -> () -> ())] = [ ]
 }
