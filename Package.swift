dc5f7bdb58ef62f8445fe7de341a321165b91639
Steve Trewick <steve.trewick@icloud.com>
2020-07-30 19:29:05 +0100

    packages

--- Package.swift
+++ Package.swift
@@ -4,25 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "RuneFactory",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+    name     : "RuneFactory",
+    platforms: [.macOS(.v10_14)],
+    products : [
         .library(
-            name: "RuneFactory",
+            name   :  "RuneFactory",
             targets: ["RuneFactory"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SteveTrewick/SourceKitHipster", from: "1.0.6"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RuneFactory",
-            dependencies: []),
+            name        : "RuneFactory",
+            dependencies: ["SourceKitHipster"]),
         .testTarget(
-            name: "RuneFactoryTests",
+            name        : "RuneFactoryTests",
             dependencies: ["RuneFactory"]),
     ]
 )
